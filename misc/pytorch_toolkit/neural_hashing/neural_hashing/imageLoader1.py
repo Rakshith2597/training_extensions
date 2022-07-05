951b3c75cf65fa6bfcbd77638dfef4a6805b59b4
Ved-kul <72691159+Ved-kul@users.noreply.github.com>
2022-07-05 16:08:15 +0530

    Update imageLoader1.py

--- misc/pytorch_toolkit/neural_hashing/neural_hashing/imageLoader1.py
+++ misc/pytorch_toolkit/neural_hashing/neural_hashing/imageLoader1.py
@@ -20,9 +20,7 @@ def find_classes(dir):
 class Dataloder_img(data.Dataset):
     def __init__(self, root, transform=None, target_transform=None):
         classes, class_to_idx = find_classes(root)
-        #samples = oversample(root)
         samples = custom_dataset(root)
-       # print(len(samples))
         self.root = root
 
         self.classes = classes
@@ -33,22 +31,12 @@ class Dataloder_img(data.Dataset):
         self.target_transform = target_transform
 
     def __getitem__(self, index):
-        # print('index:',(index))
         path1, path2, target, target1, target2 = self.samples[index]
 
         a1 = np.load(path1)
         a2 = np.load(path2)
-        # print(a1)
-        # print(a1.dtype)
-       # print(a2.dtype)
-        #sample1 = torch.from_numpy(np.load(path1))
-        #sample2 = torch.from_numpy(np.load(path2))
-       # print(sample1.dtype)
-       # print(sample2.dtype)
-        #sample1 = Image.open(sample1).convert("RGB")
 
         sample1 = Image.fromarray(a1)
-        # print(sample1)
         sample2 = Image.fromarray(a2)
         if self.transform is not None:
             sample1 = self.transform(sample1)
@@ -58,9 +46,8 @@ class Dataloder_img(data.Dataset):
             target = self.target_transform(target)
             target1 = self.target_transform(target1)
             target2 = self.target_transform(target2)
-            # c cluster_centers = self.target_transform(cluster_centers)
 
-        return sample1, sample2, target, target1, target2  # , cluster_centers
+        return sample1, sample2, target, target1, target2
 
     def __len__(self):
         return len(self.samples)
