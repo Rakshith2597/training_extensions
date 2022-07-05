1508c72458651de1fe24775d7f986efb2c528cbd
Ved-kul <72691159+Ved-kul@users.noreply.github.com>
2022-07-05 16:11:06 +0530

    Update loader_mnist.py

--- misc/pytorch_toolkit/neural_hashing/neural_hashing/loader_mnist.py
+++ misc/pytorch_toolkit/neural_hashing/neural_hashing/loader_mnist.py
@@ -4,7 +4,7 @@ import itertools
 
 
 def dataset(fpath):
-    # fpath = trainingDataPath
+
     if not os.path.exists(fpath):
         raise Exception("Invalid path destination")
     if fpath.split("/")[-1] == "train1":
@@ -22,7 +22,6 @@ def dataset(fpath):
     classes = sorted(classes)
     cls_to_idx = {classes[i]: i for i in range(len(classes))}
     print(classes)
-    # print(cls_to_idx)
     cls_num_imgs = {cls_to_idx[i]: len(os.listdir(
         os.path.join(fpath, i))) for i in classes}
     print(cls_num_imgs)
@@ -34,17 +33,11 @@ def dataset(fpath):
         images_temp = [(im, cls_name)
                        for im in os.listdir(os.path.join(fpath, cls_name))]
         random.shuffle(images_temp)
-        # images.extend(images_temp)
         images1.extend(images_temp[:200])
         images2 = images_temp[:min(im_thresh, len(images_temp))]
         dataset2.extend(list(itertools.combinations(images2, 2)))
 
-    # dataset2.extend(list(itertools.combinations(images,2)))
     dataset1 = list(itertools.combinations(images1, 2))
-    # print(len(images_temp))
-    '''print(len(images1))
-    print(len(dataset1))
-    print(len(dataset2))'''
     count = [0]*3
     newdataset = []
     for data in dataset1:
@@ -62,7 +55,6 @@ def dataset(fpath):
                 break
 
     random.shuffle(dataset2)
-    # print(dataset2)
     for data in dataset2:
         img_path1 = os.path.join(os.path.join(fpath, data[0][1]), data[0][0])
         img_path2 = os.path.join(os.path.join(fpath, data[1][1]), data[1][0])
@@ -70,20 +62,17 @@ def dataset(fpath):
         print(data[0][0].split("_")[-2])
         print(data[1][0]) 
         print(data[1][0].split("_")[-2]) '''
-        # if (((data[0][0].split("_")[0]+'_' + data[0][0].split("_")[1]) == (data[1][0].split("_")[0]+'_' + data[1][0].split("_")[1])) and (data[0][0].split("_")[-2]==data[1][0].split("_")[-2])):
+       
         if (((data[0][0].split("_")[0]+'_' + data[0][0].split("_")[1]) == (data[1][0].split("_")[0]+'_' + data[1][0].split("_")[1])) and (data[0][0].split("_")[0] == data[1][0].split("_")[0])):
             t = 0
             if count[0] < type0_num:
-                # if (t==0):
                 count[0] += 1
                 item = (img_path1, img_path2, t,
                         cls_to_idx[data[0][1]], cls_to_idx[data[1][1]])
-                # item = (data[0][0],data[1][0],t)
                 newdataset.append(item)
         else:
             t = 1
             if count[1] < type1_num:
-                # if (t==1):
                 count[1] += 1
                 item = (img_path1, img_path2, t,
                         cls_to_idx[data[0][1]], cls_to_idx[data[1][1]])
@@ -91,9 +80,4 @@ def dataset(fpath):
 
     random.shuffle(newdataset)
     print("Count", count)
-    # print(newdataset[:5])
     return newdataset
-
-#fpath = "/storage/asim/medHashing/test1"
-#newdata = dataset(fpath)
-# print(newdata)
